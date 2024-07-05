af755e56b0a01b919f318da242064991b183fd60
Md. Jubaer Mahmud Sarker <147659414+jmsman3@users.noreply.github.com>
2024-07-05 15:48:38 +0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,83 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+# Post + Edit + Delete 
+from django.shortcuts import render , redirect
+from . import forms 
+from . import models
+# Create your views here.
+# ------------------------------------------------------
+def add_post(request):
+    if request.method =='POST':
+        post_form = forms.PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect ('add_post')
+    else:
+        post_form = forms.PostForm()
+    return render(request , 'add_post.html' , {'form' : post_form})
+# ------------------------------------------------------
+
+def edit_post(request ,id):
+    post = models.Post.objects.get(pk = id)
+    post_form = forms.PostForm(instance=post)
+    if request.method =='POST':
+        post_form = forms.PostForm(request.POST , instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect ('homepage')
+    
+    return render(request , 'add_post.html' , {'form' : post_form})
+
+# ------------------------------------------------------
+def delete_post(request, id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    return redirect('homepage')
+# ------------------------------------------------------
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Practise_Day_2_Card_Food.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
-    main()
+#Category ,Profile ,author
+
+from django.shortcuts import render , redirect
+from . import forms
+# Create your views here.
+def add_category(request):
+    if request.method == 'POST': #if user click submit button 
+        category_form = forms.CategoryForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return redirect('add_category')
+    else:  # user normally website e gele blank form pabe
+        category_form = forms.CategoryForm()
+    return render ( request , 'add_category.html' , {'form' : category_form})
+
+
+# ------------------------------------------------------
+from django.shortcuts import render , redirect
+from .import forms
+# Create your views here.
+
+def add_profile(request):
+    if request.method =='POST':
+        profile_form = forms.ProfileForm(request.POST)
+        if profile_form.is_valid():
+            profile_form.save()
+            return redirect('add_profile')
+    else:
+        profile_form = forms.ProfileForm()
+    return render(request , 'add_profile.html' ,{'form' : profile_form})
+
+# ------------------------------------------------------
+
+from django.shortcuts import render , redirect
+from .import forms
+# Create your views here.
+def add_author(request):
+    if request.method == 'POST': 
+        author_form = forms.AuthorForm(request.POST)
+        if author_form.is_valid():
+            author_form.save()
+            return redirect ('add_author')
+    else:
+        author_form = forms.AuthorForm()
+    return render(request , 'add_author.html', {'form' : author_form})
